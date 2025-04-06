ac35a65062a4e4b72b422d8ab63274b45584f896
Wannes Gennar <wannes.gennar@gmail.com>
2025-04-07 01:19:50 +0200

    update custom campaign

--- src/app/custom-campaigns/page.jsx
+++ src/app/custom-campaigns/page.jsx
@@ -1,4 +1,5 @@
-import { PageIntro } from '@/components/PageIntro'
+import { PageIntro } from '@/components/PageIntro';
+import forgeHeart from '@/images/forgeheart.webp';
 
 export const metadata = {
     openGraph: {
@@ -7,7 +8,7 @@ export const metadata = {
             'Sometimes we write our own campaigns, either by ourselves or in collaboration with you',
         locale: 'en',
         images: [
-            { url: 'https://www.dealloc.be/_next/static/media/ghesia.c46f7026.webp', width: 800, height: 600 }
+            { url: forgeHeart.src, width: 800, height: 600 }
         ],
         type: 'website'
     }
@@ -16,22 +17,19 @@ export const metadata = {
 export default async function CustomCampaigns() {
     return (
         <>
-            <PageIntro eyebrow="Custom Campaigns" title="The Second Star">
+            <PageIntro eyebrow="Custom Campaigns" title="The Vault of Echoing Souls">
                 <p>
                     An Original Pathfinder 2E Campaign of Mystery and Peril
                 </p>
                 <div className="mt-10 max-w-2xl space-y-6 text-base">
                     <p>
-                        Darkness stirs in Ghesia.
-                        What begins as a desperate search for a missing girl in a troubled village soon unravels into something far greater.
-                        Bandits and beasts roam unchecked, but something far more sinister lurks beneath the surface. Whispers speak of an ancient power lost to time, a force that some would kill to claim.
-                        As the stakes rise, the choices you make will shape the fate of the land itself.
-                        Will you uncover the truth in time, or will Ghesia fall to an unseen shadow?
+                        When mysterious embers light the skies above Emberwatch, a quiet frontier town,
+                        it marks the beginning of a journey that will draw the heroes into a web of ancient betrayal.
+                        They will navigate dangerous mountain passes, uncover the truth behind a vanished expedition,
+                        and discover a long forgotten dwarven secret.
                     </p>
                     <p>
-                        <em>The Second Star</em> is a full fledged campaign written and developed by yours truly.
-                        The story is still under development, so while it&apos;s currently not available to be booked,
-                        we are open to collaboration on story, music and artwork.
+                        <em>Vault of Echoing Souls</em> is a campaign steeped in dwarven legacy, divine conflict, and the slow unraveling of a terrible past.
                     </p>
                 </div>
             </PageIntro>
